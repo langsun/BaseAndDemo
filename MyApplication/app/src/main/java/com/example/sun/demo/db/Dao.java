27204832bc5d33fdb73e4a4b88c314d6c4660b8d
langsun <lang_un@sina.com>
2016-08-10 11:01:08 +0800

    数据库的添加和查询操作

--- MyApplication/app/src/main/java/com/example/sun/demo/db/Dao.java
+++ MyApplication/app/src/main/java/com/example/sun/demo/db/Dao.java
@@ -1,6 +1,11 @@
 package com.example.sun.demo.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.sun.demo.util.Constant;
 
 /**
  * Created by sun on 16/8/8.
@@ -22,6 +27,33 @@ public class Dao {
 
     public Dao(Context context) {
         dbHelper = new DbHelper(context);
+    }
+
+    private void closeDB(SQLiteDatabase db) {
+        db.close();
+    }
+
+    private void closeCursor(Cursor cursor) {
+        cursor.close();
+    }
+
+
+    public synchronized void insertPersonData(String name, int age) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("age", age);
+        db.insert(Constant.FIRST_TABLE, null, values);
+        closeDB(db);
+    }
+
+    public synchronized int getPersonNum(String name, int age) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.query(Constant.FIRST_TABLE, new String[]{"age", "name"}, "age = ? and name = ?", new String[]{String.valueOf(age), name}, null, null, null);
+        int num = cursor.getCount();
+        closeDB(db);
+        closeCursor(cursor);
+        return num;
     }
 
 }
