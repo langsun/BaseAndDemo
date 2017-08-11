59170276252e09eb4c0f711ca490fd2a4d86ddc3
langsun <lang_un@sina.com>
2017-08-11 09:09:44 +0800

    Builder模式的使用

--- MyApplication/app/src/main/java/com/example/sun/demo/view/MyDialog.java
+++ MyApplication/app/src/main/java/com/example/sun/demo/view/MyDialog.java
@@ -1,8 +1,5 @@
 package com.example.sun.demo.view;
 
-/**
- * Created by sun on 16/2/17.
- */
 
 import android.app.Dialog;
 import android.content.Context;
@@ -17,6 +14,16 @@ import android.widget.TextView;
 
 import com.example.sun.demo.R;
 
+
+/**
+ * 使用场景：
+ * 
+ * 在设计模式中对Builder模式的定义是用于构建复杂对象的一种模式，
+ * 所构建的对象往往需要多步初始化或赋值才能完成。那么，在实际的开发过程中，
+ * 我们哪些地方适合用到Builder模式呢？其中使用Builder模式来替代多参数构造函数是一个比较好的实践法则。
+ *
+ * Created by sun on 16/2/17.
+ */
 
 public class MyDialog extends Dialog {
 
